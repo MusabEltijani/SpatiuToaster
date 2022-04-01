4e060330cc348374ef292b36bae934238f039bdb
MusabEltijani <mosaup90@gmail.com>
2022-04-01 23:19:07 +0200

    some changes

--- app/src/main/java/com/musab/toaster/MainActivity.kt
+++ app/src/main/java/com/musab/toaster/MainActivity.kt
@@ -2,10 +2,13 @@ package com.musab.toaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.spatiu.spatiutoaster.SpatiuMeassge
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SpatiuMeassge.toaster(this,"fab_transformation_sheet_behavior", 1)
     }
 }
